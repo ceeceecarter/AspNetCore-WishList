34bec3abf9b952031f1d989bbef19803da455d53
Cecilia F Carter <ceciliafcarter@gmail.com>
2021-09-27 09:48:13 -0400

    setup middleware and updated to ASPNETCORE 5

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using WishList.Data;
 
 namespace WishList
@@ -17,17 +17,23 @@ namespace WishList
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("WishList"));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer("WishList"));
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseRouting();
+            app.UseEndpoints(endpoints => {
+                endpoints.MapDefaultControllerRoute();
+            });
+
             if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
             app.UseExceptionHandler("/Home/Error");
-            app.UseMvcWithDefaultRoute();
+
+            
         }
     }
 }
